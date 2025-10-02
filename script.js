4af27ed456a6dd2d126b96f120ea792cc8f9f641
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-10-02 10:53:08 +0300

    saving work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const usersDisplay = document.querySelector(".usersDisplay");
+const createUserBtn = document.querySelector("#createUserBtn");
 
 function displayUsers(){
     usersDisplay.innerHTML = '';
@@ -16,4 +17,17 @@ function displayUsers(){
 
 }
 
-displayUsers();
+
+function createUser(){
+    usersDisplay.innerHTML+= `<div class="card user-card">
+            <div class="card-body">
+                <h5 class="card-title">User Name</h5>
+                <p class="card-text">Money owed: 2000</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`;
+}
+
+createUserBtn.addEventListener("click", () => {
+    createUser();
+})
