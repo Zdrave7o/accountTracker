ff40f1fb73af71e7f5f9385676c700000af29b09
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-10-02 17:50:54 +0300

    saving work

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 const usersDisplay = document.querySelector(".usersDisplay");
-const createUserBtn = document.querySelector("#createUserBtn");
+const createAccountBtn = document.querySelector("#createAccountBtn");
 
-function displayUsers(){
+const accounts = [];
+
+function displayUsers(accountsArr){
     usersDisplay.innerHTML = '';
 
-    for(let i = 1; i<=7; i++){
+    accountsArr.forEach(account => {
         usersDisplay.innerHTML+= `<div class="card user-card">
             <div class="card-body">
-                <h5 class="card-title">User Name</h5>
-                <p class="card-text">Money owed: 2000</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">${account.name}</h5>
+                <p class="card-text">Money owed: ${account.balance}</p>
+                <a href="#" class="btn btn-primary">Add</a>
+                <a href="#" class="btn btn-primary">Remove</a>
             </div>
         </div>`;
-    }
+    });
 
 
 }
 
 
-function createUser(){
-    usersDisplay.innerHTML+= `<div class="card user-card">
-            <div class="card-body">
-                <h5 class="card-title">User Name</h5>
-                <p class="card-text">Money owed: 2000</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>`;
+function createAccount(userName, balance){
+    
 }
 
-createUserBtn.addEventListener("click", () => {
-    createUser();
+createAccountBtn.addEventListener("click", () => {
+    displayUsers(accounts);
 })
+
+class Account{
+    constructor(name, balance){
+        this.name = name;
+        this.balance = balance;
+        this.transactions = [];
+    }
+}
+
+const user1 = new Account("Mister Cash", 2000);
+accounts.push(user1);
+
