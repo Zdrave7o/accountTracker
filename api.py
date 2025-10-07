3f0155bd85fb4c05941d8b45d2efc610fc743fed
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-10-07 10:44:17 +0300

    Created new branch for working/experimenting

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
 from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    a = 0
+    return'<h1>Hello world</h1>' 
+
+if __name__ == "__main__":
+    app.run(debug=True)
